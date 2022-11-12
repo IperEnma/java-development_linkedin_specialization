00cec04ba4086b17f2e54a558e76f1c0aae65f12
IperEnma <enmanuelhernandez1843@gmail.com>
2022-11-12 12:02:04 -0300

    basic compare

---
+++ chapter-1/src/SecondProgram.java
@@ -0,0 +1,13 @@
+public class SecondProgram {
+    public static void main(String[] args) {
+        int var1 = 5;
+        int var2 = 10;
+
+        if (var1 > var2) {
+            System.out.println("Var1 is greater than var2");
+        }
+        else {
+            System.out.println(("Var2 is greater than var1"));
+        }
+    }
+}
