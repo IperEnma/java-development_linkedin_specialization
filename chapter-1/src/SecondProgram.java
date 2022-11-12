cd6243481e9489996d43ff0e9193084e9837ca4e
IperEnma <enmanuelhernandez1843@gmail.com>
2022-11-12 13:22:02 -0300

    practice switch and scanner

--- chapter-1/src/SecondProgram.java
+++ chapter-1/src/SecondProgram.java
@@ -1,13 +1,35 @@
+import java.util.Scanner;
+
 public class SecondProgram {
     public static void main(String[] args) {
-        int var1 = 5;
-        int var2 = 10;
+        Scanner scanner = new Scanner(System.in);
+        loop: while (true) {
+            System.out.println("=========================");
+            System.out.println("1) compare numbers");
+            System.out.println("2) exit");
+            int option = scanner.nextInt();
+            System.out.println("=========================");
 
-        if (var1 > var2) {
-            System.out.println("Var1 is greater than var2");
-        }
-        else {
-            System.out.println(("Var2 is greater than var1"));
+            switch (option) {
+                case 1: {
+                    System.out.println("Ingrese numero: ");
+                    int var1 = scanner.nextInt();
+                    System.out.println("Ingrese numero: ");
+                    int var2 = scanner.nextInt();
+
+                    if (var1 > var2) {
+                        System.out.println(var1 + " is greater than " + var2 + "\n");
+                    } else if (var1 < var2) {
+                        System.out.println(var2 + " is greater than " + var1 + "\n");
+                    } else {
+                        System.out.println(var2 + " equals " + var1 + "\n");
+                    }
+                    break;
+                }
+                case 2:{
+                    break loop;
+                }
+            }
         }
     }
 }
