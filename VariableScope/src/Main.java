f85b5f4e15fc0139fa0b1340295062df84e76de2
IperEnma <enmanuelhernandez1843@gmail.com>
2022-11-14 18:32:51 -0300

    test variable scope

---
+++ VariableScope/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int dias = 0;
+        int meses = 0;
+
+        while (dias < 30) {
+            dias = dias + 1;
+            int semanas = dias / 7;
+            meses = meses + 1;
+            System.out.println("Han pasado " + dias + " días, lo que supone " + semanas + " semanas");
+            System.out.println("Han pasado " + meses + " meses");
+        }
+    }
+}
